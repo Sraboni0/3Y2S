c034f142aacea3a571b879e97456ee10a0c30b5a
Sohanur Rahman <israkcse@gmail.com>
2020-03-03 02:34:26 +0600

    UPDATE

--- Assembly_Lab/Final LAB EXAM Code For 8086/Factorial.asm
+++ Assembly_Lab/Final LAB EXAM Code For 8086/Factorial.asm
@@ -10,59 +10,36 @@ MAIN PROC
     MOV AX,@DATA
     MOV DS,AX
     
-    PRINTN "Enter a number:"
+    PRINTN "ENTER INPUT NUMBER: "
+    
     MOV AH,1
     INT 21H
-    SUB AL,48
-    
     CMP AL,0
-    JE ZERO   
+    JE ZERO
+    SUB AL,48
+    INPUT:
         MOV N,AL
         DEC N
         FOR_LOOP:
             CMP N,0
-            JE ZERO
+            JE PRNT
             MUL N
             DEC N
             JMP FOR_LOOP
-        ZERO:
-       MOV SUM,AL
-       ;ADD SUM,48 
+     
+     ZERO:
+        MOV AL,1
+     PRNT:  
         PRINTN
-        ;CALL NEWLINE
-        AAM  ;adjust ascii
-                
-        MOV CH,AH  ;upper part store in ch
-        MOV CL,AL
-        
-        MOV DL,CH ;Move first part of mul to DL register
-        ADD DL,48
-        MOV AH,2     ;Print first digit of multiplication
-        INT 21H   
-        
-        MOV DL,CL
-        ADD DL,48
-        MOV AH,2  ;Print 2nd digit of multiplication
-        INT 21H
-       
-       ;without Ascii Adjust
-       CALL NEWLINE
-       PRINTN "IN Ascii: "
-       MOV AH,2
-       MOV DL,SUM
-       INT 21H 
-       
+        PRINTN "OUTPUT: "
+        ADD AL,48
+               
+        MOV AH,2
+        MOV DL,AL
+        INT 21H  
+           
     MOV AH, 4CH
     INT 21H
     
     MAIN ENDP
-    
-    NEWLINE PROC
-        MOV AH,2
-        MOV DL,0AH
-        INT 21H
-        MOV DL,0DH
-        INT 21H
-        RET    
-    NEWLINE ENDP
 END MAIN
